3b523400094ce7fd2b5a7c6d2fdc9087bd556475
Simon Pratt <spratt@scs.carleton.ca>
2012-07-24 15:14:33 -0400

    Now enumerating properly

--- enumerate_dice.py
+++ enumerate_dice.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python2.6
 
-# constants
+import sys
+
+# defaults
 NUM_DICE=4
 DICE_SIDES=6
 
-# variables
-val=1
-d=[]
+# parameters
+if len(sys.argv) > 1:
+    NUM_DICE=int(sys.argv[1])
+if len(sys.argv) > 2:
+    DICE_SIDES=int(sys.argv[2])
+ 
+# utility functions
+def print_dice(dice):
+    for die in dice:
+        print str(die)+'\t',
+    print
 
-# initialize dice
-for i in range(1,NUM_DICE):
-    d.append(val)
+def enumerate_dice(n):
+    if n==1:
+        return [[i] for i in range(1,DICE_SIDES+1)]
+    sublist=enumerate_dice(n-1)
+    return [l+[i] for l in sublist for i in range(1,DICE_SIDES+1)]
 
-# enumerate events
-while val <= DICE_SIDES:
-    print val
-    val += 1
+# enumerate!!
+for dice in enumerate_dice(NUM_DICE):
+    print_dice(dice)
+
